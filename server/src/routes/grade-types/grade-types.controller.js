4015d00e04199ca8b3ae5c9fff9a47ce8bdde1cb
Pawel Bak <pawelbak300589@gmail.com>
2022-07-01 19:21:08 +0100

    Create endpoint to get grade-types by gradeTypeID

--- server/src/routes/grade-types/grade-types.controller.js
+++ server/src/routes/grade-types/grade-types.controller.js
@@ -15,6 +15,22 @@ async function httpGetAllGradeTypes(req, res, next) {
   }
 };
 
+async function httpGetGradeTypeById(req, res, next) {
+  try {
+    const gradeTypeId = req.params.gradeTypeID;
+
+    const gradeType = await GradeType.findByPk(gradeTypeId);
+
+    if (!gradeType) throw 'Grade type not found';
+
+    return res.status(200).json(gradeType);
+
+  } catch (error) {
+    next(error);
+  }
+};
+
 module.exports = {
   httpGetAllGradeTypes,
+  httpGetGradeTypeById,
 };
--- server/src/routes/grade-types/grade-types.router.js
+++ server/src/routes/grade-types/grade-types.router.js
@@ -2,10 +2,12 @@ const express = require('express');
 
 const {
   httpGetAllGradeTypes,
+  httpGetGradeTypeById,
 } = require('./grade-types.controller');
 
 const gradeTypesRouter = express.Router();
 
 gradeTypesRouter.get('/', httpGetAllGradeTypes);
+gradeTypesRouter.get('/:gradeTypeID', httpGetGradeTypeById);
 
 module.exports = gradeTypesRouter;
