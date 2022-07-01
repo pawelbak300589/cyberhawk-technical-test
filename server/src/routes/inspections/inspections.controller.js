d6ad1861d4f5aa0e9604262a4f8de01083963778
Pawel Bak <pawelbak300589@gmail.com>
2022-07-01 18:31:41 +0100

    Create endpoint to get inspection by inspectionID

--- server/src/routes/inspections/inspections.controller.js
+++ server/src/routes/inspections/inspections.controller.js
@@ -1,4 +1,5 @@
 const Inspection = require('../../db/models/Inspection');
+const Grade = require('../../db/models/Grade');
 
 async function httpGetAllInspections(req, res, next) {
   try {
@@ -15,6 +16,22 @@ async function httpGetAllInspections(req, res, next) {
   }
 };
 
+async function httpGetInspectionById(req, res, next) {
+  try {
+    const inspectionId = req.params.inspectionID;
+
+    const inspection = await Inspection.findByPk(inspectionId);
+
+    if (!inspection) throw 'Inspection not found';
+
+    return res.status(200).json(inspection);
+
+  } catch (error) {
+    next(error);
+  }
+};
+
 module.exports = {
   httpGetAllInspections,
+  httpGetInspectionById,
 };
--- server/src/routes/inspections/inspections.router.js
+++ server/src/routes/inspections/inspections.router.js
@@ -2,10 +2,12 @@ const express = require('express');
 
 const {
   httpGetAllInspections,
+  httpGetInspectionById,
 } = require('./inspections.controller');
 
 const inspectionsRouter = express.Router();
 
 inspectionsRouter.get('/', httpGetAllInspections);
+inspectionsRouter.get('/:inspectionID', httpGetInspectionById);
 
 module.exports = inspectionsRouter;
