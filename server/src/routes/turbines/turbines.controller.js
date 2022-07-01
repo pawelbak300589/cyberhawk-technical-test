23f83e402116e46b968d588550f9d01786fd0180
Pawel Bak <pawelbak300589@gmail.com>
2022-07-01 17:48:45 +0100

    Create endpoint to get all components by turbineID

--- server/src/routes/turbines/turbines.controller.js
+++ server/src/routes/turbines/turbines.controller.js
@@ -1,4 +1,5 @@
 const Turbine = require('../../db/models/Turbine');
+const Component = require('../../db/models/Component');
 
 async function httpGetAllTurbines(req, res, next) {
   try {
@@ -30,7 +31,25 @@ async function httpGetTurbineById(req, res, next) {
   }
 };
 
+async function httpGetComponentsByTurbineId(req, res, next) {
+  try {
+    const turbineId = req.params.turbineID;
+
+    const turbine = await Turbine.findByPk(turbineId, { include: Component });
+
+    if (!turbine) throw 'Turbine not found';
+
+    return res.status(200).json({
+      data: turbine.components ?? []
+    });
+
+  } catch (error) {
+    next(error);
+  }
+};
+
 module.exports = {
   httpGetAllTurbines,
   httpGetTurbineById,
+  httpGetComponentsByTurbineId,
 };
--- server/src/routes/turbines/turbines.router.js
+++ server/src/routes/turbines/turbines.router.js
@@ -3,11 +3,13 @@ const express = require('express');
 const {
   httpGetAllTurbines,
   httpGetTurbineById,
+  httpGetComponentsByTurbineId,
 } = require('./turbines.controller');
 
 const turbinesRouter = express.Router();
 
 turbinesRouter.get('/', httpGetAllTurbines);
 turbinesRouter.get('/:turbineID', httpGetTurbineById);
+turbinesRouter.get('/:turbineID/components', httpGetComponentsByTurbineId);
 
 module.exports = turbinesRouter;
