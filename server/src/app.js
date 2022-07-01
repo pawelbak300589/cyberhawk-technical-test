05c521cd8c79ef73dd1d7c92644f7bf44f94a1f8
Pawel Bak <pawelbak300589@gmail.com>
2022-07-01 17:26:15 +0100

    Use error handler middleware in the app

--- server/src/app.js
+++ server/src/app.js
@@ -1,8 +1,13 @@
 const express = require('express');
 
 require('./db/models/associations')();
+
+const errorHandler = require('./middleware/errorHandler');
+
 const app = express();
 
 app.use(express.json());
+
+app.use(errorHandler);
 
 module.exports = app;
