3167235f451a2ab4cecdae557140e9937b338a4c
Pawel Bak <pawelbak300589@gmail.com>
2022-07-01 17:19:52 +0100

    Create models associations file

---
+++ server/src/db/models/associations.js
@@ -0,0 +1,21 @@
+const Farm = require('./Farm');
+const Turbine = require('./Turbine');
+const ComponentType = require('./ComponentType');
+const Component = require('./Component');
+const GradeType = require('./GradeType');
+const Grade = require('./Grade');
+const Inspection = require('./Inspection');
+
+const setAssociations = () => {
+  Farm.hasMany(Turbine, { foreignKey: 'farm_id' });
+  Turbine.belongsTo(Farm, { foreignKey: 'id' });
+  Turbine.hasMany(Component, { foreignKey: 'turbine_id' });
+  Turbine.hasMany(Inspection, { foreignKey: 'turbine_id' });
+  Component.belongsTo(Turbine, { foreignKey: 'id' });
+  Component.hasOne(ComponentType, { foreignKey: 'component_type_id' });
+  Inspection.belongsTo(Turbine, { foreignKey: 'id' });
+  Grade.belongsTo(Inspection, { foreignKey: 'id' });
+  Grade.belongsTo(Component, { foreignKey: 'id' });
+  Grade.hasOne(GradeType, { foreignKey: 'grade_type_id' });
+};
+module.exports = setAssociations;
