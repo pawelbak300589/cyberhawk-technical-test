fb98742c24478a361d5d6d0034c72294c005e22e
Pawel Bak <pawelbak300589@gmail.com>
2022-07-02 16:45:42 +0100

    Create custom list item component

---
+++ client/src/components/CustomListItem.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import { Link } from "react-router-dom";
+import { styled, ListItem, ListItemText, ListItemAvatar, Avatar, IconButton } from '@mui/material';
+import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
+
+const StyledListItem = styled(ListItem)`
+  border: 1px solid #eeeeee;
+  margin-bottom: 5px;
+  transition: background 300ms ease-in-out;
+  :hover {
+    background: #eeeeee;
+  }
+`;
+
+const CustomListItem = ({ Icon, title, subtitle, link }) => {
+  return (
+    <StyledListItem
+      secondaryAction={
+        <IconButton edge="end" aria-label="show" component={Link} to={link}>
+          <ArrowForwardIosIcon />
+        </IconButton>
+      }>
+      <ListItemAvatar>
+        <Avatar>
+          <Icon />
+        </Avatar>
+      </ListItemAvatar>
+      <ListItemText primary={title} secondary={subtitle} />
+    </StyledListItem>
+  );
+};
+
+export default CustomListItem;
