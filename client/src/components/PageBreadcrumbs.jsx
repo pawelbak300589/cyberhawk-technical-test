e9e070a6139721e072389dc72a4cfa0c87038a2f
Pawel Bak <pawelbak300589@gmail.com>
2022-07-02 16:44:41 +0100

    Create page breadcrumbs component to display the breadcrumbs

---
+++ client/src/components/PageBreadcrumbs.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Link as RouterLink } from "react-router-dom";
+import { Breadcrumbs, Typography, Link } from '@mui/material';
+
+const PageBreadcrumbs = ({ breadcrumbs }) => {
+  return breadcrumbs && (
+    <Breadcrumbs maxItems={2} aria-label="breadcrumb">
+      <Link underline="hover" color="inherit" component={RouterLink} to="/">
+        Home
+      </Link>
+      {
+        breadcrumbs.map((breadcrumb) => "link" in breadcrumb ? (
+          <Link underline="hover" color="inherit" component={RouterLink} to={breadcrumb.link}>
+            {breadcrumb.text}
+          </Link>
+        ) : (
+          <Typography color="text.primary">{breadcrumb.text}</Typography>
+        ))
+      }
+    </Breadcrumbs>
+  );
+};
+
+export default PageBreadcrumbs;
