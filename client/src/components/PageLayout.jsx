19ecbe2fecf44c26b9913f4334787bb3321587a2
Pawel Bak <pawelbak300589@gmail.com>
2022-07-02 16:46:26 +0100

    Use page breadcrumbs in the page layout

--- client/src/components/PageLayout.jsx
+++ client/src/components/PageLayout.jsx
@@ -3,14 +3,18 @@ import { Box, Container, Typography } from '@mui/material';
 
 import Navbar from './Navbar';
 import PageLoader from './PageLoader';
+import PageBreadcrumbs from './PageBreadcrumbs';
 
-const PageLayout = ({ children, title, loading }) => {
+const PageLayout = ({ children, title, loading, breadcrumbs }) => {
   return (
     <Box>
       <Navbar />
       <Container>
         <Box mt={4}>
-          <Typography variant="h4" gutterBottom>{title}</Typography>
+          <PageBreadcrumbs breadcrumbs={breadcrumbs} />
+          <Box my={1}>
+            <Typography variant="h4" gutterBottom>{title}</Typography>
+          </Box>
           {loading ? <PageLoader /> : children}
         </Box>
       </Container>
